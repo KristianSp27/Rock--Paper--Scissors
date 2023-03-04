937710785713642edbc654d5e3b15b854cd65af7
KristianSp27 <101999488+KristianSp27@users.noreply.github.com>
2023-03-04 22:56:53 +0200

    Update

---
+++ script.js
@@ -0,0 +1,46 @@
+const selectionButtons = document.querySelectorAll("[data-selection]");
+const SELECTIONS = [
+  {
+    name: "rock",
+    emoji: "✊",
+    beats: "scissors",
+  },
+  {
+    name: "paper",
+    emoji: "🤚",
+    beats: "rock",
+  },
+  {
+    name: "scissors",
+    emoji: "✌️",
+    beats: "paper",
+  },
+];
+
+selectionButtons.forEach((selectionButton) => {
+  selectionButton.addEventListener("clic", (e) => {
+    const selectionName = selectionButton.dataset.selelction;
+    const selection = SELECTIONS.find((selection) => selection.name === selectionName);
+    makeSelection(selection);
+  });
+});
+
+function makeSelection(selection) {
+  const computerSelection = randomSelection();
+  const yourWinner = isWinner(selection, computerSelection);
+  const computerWinner = isWinner(computerSelection, selection);
+
+  addSelectionResult(computerSelection, computerWinner);
+  addSelectionResult(selection, yourWinner);
+}
+
+function addSelectionResult(selection, winner) {}
+
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name;
+}
+
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+  return SELECTIONS[randomIndex];
+}
